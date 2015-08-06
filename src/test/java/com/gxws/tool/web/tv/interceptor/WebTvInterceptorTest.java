cac67af2fdb35d553330eeb4ac2730f15a5833bb
朱伟亮 <zhuwl120820@gxwsxx.com>
2015-08-06 18:01:34 +0800

    添加根据机顶盒类型的测试

--- src/test/java/com/gxws/tool/web/tv/interceptor/WebTvInterceptorTest.java
+++ src/test/java/com/gxws/tool/web/tv/interceptor/WebTvInterceptorTest.java
@@ -34,68 +34,122 @@ public class WebTvInterceptorTest extends AbstractTestNGSpringContextTests {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
 	}
 
+	/**
+	 * 完整参数
+	 * 
+	 * @author zhuwl120820@gxwsxx.com
+	 * @throws Exception
+	 *             异常
+	 * @since 1.1
+	 */
 	@Test
 	public void preHandleWithParam() throws Exception {
-		MvcResult result = mockMvc
-				.perform(MockMvcRequestBuilders.get("/testurl1?stbId=123&dvbId=213&areaId=321&stbType=0001"))
+		String param = "stbId=123&dvbId=213&areaId=321&stbType=0003";
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/testurl1?" + param))
 				.andDo(MockMvcResultHandlers.print()).andReturn();
 		Assert.assertNotNull(result.getModelAndView().getViewName());
-		WebTvParam p =  (WebTvParam) result.getRequest().getAttribute(WebTvParam.ATTR_NAME);
-		String value =  p.getUrl();
-		Assert.assertEquals(value, "&stbId=123&dvbId=213&areaId=321&stbType=0001");
+		WebTvParam p = (WebTvParam) result.getRequest().getAttribute(WebTvParam.ATTR_NAME);
+		Assert.assertEquals(p.getStbId(), "123");
+		Assert.assertEquals(p.getDvbId(), "213");
+		Assert.assertEquals(p.getAreaId(), "321");
+		Assert.assertEquals(p.getStbType(), "0003");
+		Assert.assertEquals(p.getUrl(), "&" + param);
 	}
 
 	@Test
-	public void preHandleWithoutParam0() throws Exception {
+	public void preHandleMissingParam0() throws Exception {
 		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/testurl1")).andDo(MockMvcResultHandlers.print())
 				.andReturn();
 		Assert.assertNull(result.getModelAndView());
 	}
 
 	@Test
-	public void preHandleWithoutParam1() throws Exception {
+	public void preHandleMissingParam1() throws Exception {
 		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/testurl1?stbId=123"))
 				.andDo(MockMvcResultHandlers.print()).andReturn();
 		Assert.assertNull(result.getModelAndView());
 	}
 
 	@Test
-	public void preHandleWithoutParam2() throws Exception {
+	public void preHandleMissingParam2() throws Exception {
 		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/testurl1?stbId=123&dvbId=213"))
 				.andDo(MockMvcResultHandlers.print()).andReturn();
 		Assert.assertNull(result.getModelAndView());
 	}
 
 	@Test
-	public void preHandleWithoutParam3() throws Exception {
+	public void preHandleMissingParam3() throws Exception {
 		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/testurl1?stbId=123&dvbId=213&areaId=321"))
 				.andDo(MockMvcResultHandlers.print()).andReturn();
-		Assert.assertNotNull(result.getModelAndView().getViewName());
+		Assert.assertNull(result.getModelAndView());
 	}
 
 	@Test
-	public void preHandleWithoutParam4() throws Exception {
+	public void preHandleMissingParam4() throws Exception {
 		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/testurl1?device_id=123"))
 				.andDo(MockMvcResultHandlers.print()).andReturn();
 		Assert.assertNull(result.getModelAndView());
 	}
 
 	@Test
-	public void preHandleWithoutParam5() throws Exception {
+	public void preHandleMissingParam5() throws Exception {
 		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/testurl1?device_id=123&user_id=213"))
 				.andDo(MockMvcResultHandlers.print()).andReturn();
 		Assert.assertNull(result.getModelAndView());
 	}
 
 	@Test
-	public void preHandleWithoutParam6() throws Exception {
-		MvcResult result = mockMvc
-				.perform(MockMvcRequestBuilders.get("/testurl1?device_id=123&user_id=213&area_code=321&stbType=0002"))
+	public void preHandleMissingParam6() throws Exception {
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+				.get("/testurl1?device_id=123&user_id=213&area_code=321&stbType=0002").header("User-Agent", "abc"))
 				.andDo(MockMvcResultHandlers.print()).andReturn();
 		Assert.assertNotNull(result.getModelAndView().getViewName());
-		WebTvParam p =  (WebTvParam) result.getRequest().getAttribute(WebTvParam.ATTR_NAME);
-		String value =  p.getUrl();
-		Assert.assertEquals(value, "&stbId=123&dvbId=213&areaId=321&stbType=0002");
+		WebTvParam p = (WebTvParam) result.getRequest().getAttribute(WebTvParam.ATTR_NAME);
+		Assert.assertEquals(p.getStbId(), "123");
+		Assert.assertEquals(p.getDvbId(), "213");
+		Assert.assertEquals(p.getAreaId(), "321");
+		Assert.assertEquals(p.getStbType(), "0001");
+		Assert.assertEquals(p.getUrl(), "&stbId=123&dvbId=213&areaId=321&stbType=0001");
+	}
+
+	@Test
+	public void preHandleMissingParam61() throws Exception {
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+				.get("/testurl1?device_id=123&user_id=213&area_code=321&device_type=0002").header("User-Agent", "abc"))
+				.andDo(MockMvcResultHandlers.print()).andReturn();
+		Assert.assertNotNull(result.getModelAndView().getViewName());
+		WebTvParam p = (WebTvParam) result.getRequest().getAttribute(WebTvParam.ATTR_NAME);
+		Assert.assertEquals(p.getStbId(), "123");
+		Assert.assertEquals(p.getDvbId(), "213");
+		Assert.assertEquals(p.getAreaId(), "321");
+		Assert.assertEquals(p.getStbType(), "0001");
+		Assert.assertEquals(p.getUrl(), "&stbId=123&dvbId=213&areaId=321&stbType=0001");
+	}
+
+	@Test
+	public void preHandleMissingParam7() throws Exception {
+		MvcResult result = mockMvc
+				.perform(MockMvcRequestBuilders.get("/testurl1?device_id=123&user_id=213&area_code=321&stbType=0002")
+						.header("User-Agent",
+								"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.107 Safari/537.36"))
+				.andDo(MockMvcResultHandlers.print()).andReturn();
+		Assert.assertNotNull(result.getModelAndView().getViewName());
+		WebTvParam p = (WebTvParam) result.getRequest().getAttribute(WebTvParam.ATTR_NAME);
+		Assert.assertEquals(p.getStbId(), "123");
+		Assert.assertEquals(p.getDvbId(), "213");
+		Assert.assertEquals(p.getAreaId(), "321");
+		Assert.assertEquals(p.getStbType(), "0002");
+		Assert.assertEquals(p.getUrl(), "&stbId=123&dvbId=213&areaId=321&stbType=0002");
+	}
+
+	@Test
+	public void preHandleIllegalParam() throws Exception {
+		MvcResult result = mockMvc
+				.perform(MockMvcRequestBuilders.get("/testurl1?device_id=123&user_id=213&area_code=321&stbType=0004")
+						.header("User-Agent",
+								"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.107 Safari/537.36"))
+				.andDo(MockMvcResultHandlers.print()).andReturn();
+		Assert.assertNull(result.getModelAndView());
 	}
 
 }
